e22043c5789b6fedbfd9cf83b35334d5ae358ddb
SaintEfim <saintefim2@gmail.com>
2024-06-07 16:50:13 +0300

    added new model

---
+++ src/WC.Library.Web/Models/CreateActionResultDto.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WC.Library.Web.Models;
+
+public class CreateActionResultDto : IDto
+{
+    [Required]
+    public Guid Id { get; init; }
+}
